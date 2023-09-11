6058ad2b76aa2cdcc4654abc42c23711019c0830
omkarmore2008 <omkarmore1100@gmail.com>
2023-09-11 13:39:05 +0530

    Create-cloud function

--- cloud-functions/event_handlers.py
+++ cloud-functions/event_handlers.py
@@ -1,1 +1,21 @@
+from cloudevents.http import CloudEvent
+import functions_framework
+from google.events.cloud import firestore
 
+
+@functions_framework.cloud_event
+def create_data(cloud_event: CloudEvent) -> None:
+    """Triggers by a change to a Firestore document.
+    Args:
+        cloud_event: cloud event with information on the firestore event trigger
+    """
+    firestore_payload = firestore.DocumentEventData()
+    firestore_payload._pb.ParseFromString(cloud_event.data)
+
+    print(f"Function triggered by change to: {cloud_event['source']}")
+
+    print("\nOld value:")
+    print(firestore_payload.old_value)
+
+    print("\nNew value:")
+    print(firestore_payload.value)
